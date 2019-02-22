bb13777be8eb200a1fce7c0017bd502ad31ecfd6
alan <a.ikaev@elonsoft.ru>
2019-02-22 09:26:21 +0300

    Update readme.md and delete binary files

--- lab1Vic31/lab1Vic31/Program.cs
+++ lab1Vic31/lab1Vic31/Program.cs
@@ -11,6 +11,14 @@ namespace lab1Vic31
             
             Console.WriteLine("Мурсалимов А.Т.");
             Console.WriteLine("Семенов И.А.");
+            Console.WriteLine("Абарин В.В.");
+            
+            Console.WriteLine("Документацией занимались: ");
+            Console.WriteLine("Икаев А.С.");
+            Console.WriteLine("Кравченко В.Д.");
+            Console.WriteLine("Касимов И.А.");
+            Console.WriteLine("Белов И.О.");
+            Console.WriteLine("Ворожев Я.А.");
         }
     }
 }
