64b1241e4be00c6291274de1e9a08c48f7627dd0
esco8800 <47655755+esco8800@users.noreply.github.com>
2019-02-22 09:05:57 +0300

    Add files via upload

---
+++ lab1Vic31/lab1Vic31/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lab1Vic31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тестовая строка");
+            Console.WriteLine("Разработкой занимались:");
+        }
+    }
+}
